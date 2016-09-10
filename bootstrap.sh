ecb89a62610465073868772247fa8e0720c1f6aa
I can not only fetch JSON, but parse it too <Dog@users.noreply.github.com>
2016-09-09 17:49:33 -0700

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -8,6 +8,7 @@ function doIt() {
 	rsync --exclude ".git/" \
 		--exclude ".DS_Store" \
 		--exclude "bootstrap.sh" \
+		--exclude ".macos" \
 		--exclude "README.md" \
 		--exclude "LICENSE-MIT.txt" \
 		-avh --no-perms . ~;
@@ -16,6 +17,7 @@ function doIt() {
 
 if [ "$1" == "--force" -o "$1" == "-f" ]; then
 	doIt;
+	./.macos;
 else
 	read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1;
 	echo "";
