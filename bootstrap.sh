7e980be6c756f42a41e2bcb139730defe58b2be4
I can not only fetch JSON, but parse it too <Dog@users.noreply.github.com>
2016-09-09 18:14:17 -0700

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -18,18 +18,18 @@ function doIt() {
 }
 
 if [ "$1" == "--force" -o "$1" == "-f" ]; then
-	doIt;
-	./.macos;
+	sudo ./.macos;
 	brew bundle Brewfile;
 	brew bundle Caskfile;
+	doIt;
 else
 	read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1;
 	echo "";
 	if [[ $REPLY =~ ^[Yy]$ ]]; then
-		doIt;
 		./.macos;
 		brew bundle Brewfile;
 		brew bundle Caskfile;
+		doIt;
 	fi;
 fi;
 unset doIt;
